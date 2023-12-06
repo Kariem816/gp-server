28010abb01fd5f4a6095253f733439c59ce7f5ed
Kariem816 <kariem816@gmail.com>
2023-12-06 17:18:08 +0200

    fix problem with UT callback

--- src/routers/api/uploads.router.ts
+++ src/routers/api/uploads.router.ts
@@ -1,10 +1,16 @@
-import { Router } from "express";
+import { Router, json } from "express";
 
 import { createUploadthingExpressHandler } from "uploadthing/express";
 
 import { uploadRouter } from "@/config/uploads";
 
 const router = Router();
+
+// Why can't UploadThing send correct content-type header?
+router.use((req, res, next) => {
+	req["headers"]["content-type"] = "application/json";
+	next();
+}, json());
 
 router.use(
 	createUploadthingExpressHandler({

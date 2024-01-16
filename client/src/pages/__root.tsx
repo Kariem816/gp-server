893ba44831c965d490d1c1b2a84f680d597ce08d
Kariem816 <kariem816@gmail.com>
2024-01-16 03:34:59 +0200

    C: setup basic app shell

--- client/src/pages/__root.tsx
+++ client/src/pages/__root.tsx
@@ -1,15 +1,105 @@
-import React from "react"; // TODO: Find a way to remove this import (tsconfig.json)
-import { Outlet, rootRouteWithContext } from "@tanstack/react-router";
+import { Link, Outlet, rootRouteWithContext } from "@tanstack/react-router";
 import { RouterContext } from "../routerContext";
+import {
+	AppShell,
+	Avatar,
+	Burger,
+	Button,
+	Group,
+	UnstyledButton,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { useAuth } from "~/contexts/auth";
+import Logo from "~/components/logo";
+
+import classes from "~/styles/index.module.css";
+import { SignedIn, SignedOut } from "~/components/auth";
 
 export const Route = rootRouteWithContext<RouterContext>()({
 	component: RootComponent,
 });
 
 function RootComponent() {
+	const [opened, { toggle }] = useDisclosure();
+	const { logout, user } = useAuth();
+
 	return (
-		<>
-			<Outlet />
-		</>
+		<AppShell
+			header={{ height: 60 }}
+			navbar={{
+				width: 300,
+				breakpoint: "sm",
+				collapsed: { desktop: true, mobile: !opened },
+			}}
+			padding="md"
+		>
+			<AppShell.Header>
+				<Group h="100%" px="md">
+					<Burger
+						opened={opened}
+						onClick={toggle}
+						hiddenFrom="sm"
+						size="sm"
+					/>
+					<Group justify="space-between" style={{ flex: 1 }}>
+						<Logo size={50} />
+						<Group ml="xl" gap="md" visibleFrom="sm">
+							<UnstyledButton
+								className={classes.control}
+								component={Link}
+								to="/"
+							>
+								Home
+							</UnstyledButton>
+							<SignedIn>
+								<Link to="/profile/me">
+									<Avatar
+										// @ts-ignore
+										src={user?.img}
+										// @ts-ignore
+										alt={user?.username}
+									/>
+								</Link>
+								<Button
+									className={classes.control}
+									variant="outline"
+									onClick={logout}
+								>
+									Logout
+								</Button>
+							</SignedIn>
+							<SignedOut>
+								<Button
+									className={classes.control}
+									component={Link}
+									to="/login"
+								>
+									Login
+								</Button>
+							</SignedOut>
+						</Group>
+					</Group>
+				</Group>
+			</AppShell.Header>
+
+			<AppShell.Navbar py="md" px={4}>
+				<UnstyledButton className={classes.control}>
+					Home
+				</UnstyledButton>
+				<UnstyledButton className={classes.control}>
+					Blog
+				</UnstyledButton>
+				<UnstyledButton className={classes.control}>
+					Contacts
+				</UnstyledButton>
+				<UnstyledButton className={classes.control}>
+					Support
+				</UnstyledButton>
+			</AppShell.Navbar>
+
+			<AppShell.Main>
+				<Outlet />
+			</AppShell.Main>
+		</AppShell>
 	);
 }
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,75 +1,183 @@
 import { FileRoute } from "@tanstack/react-router";
-import { AppShell, Burger, Group, UnstyledButton } from "@mantine/core";
-import { useDisclosure } from "@mantine/hooks";
-import Logo from "~/components/logo";
-
-import classes from "~/styles/index.module.css";
+import { UnstyledButton, Code, List, Title, Text, Space } from "@mantine/core";
 
 export const Route = new FileRoute("/").createRoute({
 	component: Page,
 });
 
 export function Page() {
-	const [opened, { toggle }] = useDisclosure();
-
 	return (
-		<AppShell
-			header={{ height: 60 }}
-			navbar={{
-				width: 300,
-				breakpoint: "sm",
-				collapsed: { desktop: true, mobile: !opened },
-			}}
-			padding="md"
-		>
-			<AppShell.Header>
-				<Group h="100%" px="md">
-					<Burger
-						opened={opened}
-						onClick={toggle}
-						hiddenFrom="sm"
-						size="sm"
-					/>
-					<Group justify="space-between" style={{ flex: 1 }}>
-						<Logo size={50} />
-						<Group ml="xl" gap={0} visibleFrom="sm">
-							<UnstyledButton className={classes.control}>
-								Home
-							</UnstyledButton>
-							<UnstyledButton className={classes.control}>
-								Blog
-							</UnstyledButton>
-							<UnstyledButton className={classes.control}>
-								Contacts
-							</UnstyledButton>
-							<UnstyledButton className={classes.control}>
-								Support
-							</UnstyledButton>
-						</Group>
-					</Group>
-				</Group>
-			</AppShell.Header>
-
-			<AppShell.Navbar py="md" px={4}>
-				<UnstyledButton className={classes.control}>
-					Home
-				</UnstyledButton>
-				<UnstyledButton className={classes.control}>
-					Blog
-				</UnstyledButton>
-				<UnstyledButton className={classes.control}>
-					Contacts
-				</UnstyledButton>
-				<UnstyledButton className={classes.control}>
-					Support
-				</UnstyledButton>
-			</AppShell.Navbar>
-
-			<AppShell.Main>
-				Navbar is only visible on mobile, links that are rendered in the
-				header on desktop are hidden on mobile in header and rendered in
-				navbar instead.
-			</AppShell.Main>
-		</AppShell>
+		<>
+			<Title my="md" order={1}>
+				IoT Smart Campus
+			</Title>
+			<Text>
+				This is the website for the IoT Smart Campus project. The
+				project is part of courses{" "}
+				<Code>ECE491 - Graduation Project I</Code> and{" "}
+				<Code>ECE492 - Graduation Project II</Code> at Faculty of
+				Engineering, Ain Shams University.
+			</Text>
+			<Title my="md" order={2}>
+				Project Description
+			</Title>
+			<Text>
+				The project aims to build a smart campus that can be used to
+				monitor the campus and provide useful information to the
+				students and staff. The project is divided into three main
+				parts:
+			</Text>
+			<List>
+				<List.Item>Embedded</List.Item>
+				<List.Item>Server</List.Item>
+				<List.Item>Image Processing</List.Item>
+			</List>
+			<Title my="md" order={3}>
+				Embedded
+			</Title>
+			<Text>
+				The embedded part is responsible for collecting data from the
+				sensors and sending it to the server. The embedded part is built
+				using Arduino and ESP8266.
+			</Text>
+			<Title my="md" order={3}>
+				Server
+			</Title>
+			<Text>
+				The server is responsible for receiving data from the embedded
+				part and storing it in a database. The server is also
+				responsible for providing the data to the image processing part
+				and the web/mobile application.
+			</Text>
+			<Title my="md" order={3}>
+				Image Processing
+			</Title>
+			<Text>
+				The image processing part is responsible for processing the
+				images received from the server and detecting the number of
+				people in the image. The image processing part is built using
+				Python and OpenCV.
+			</Text>
+			<Title my="md" order={2}>
+				Contributors
+			</Title>
+			<Title my="md" order={3}>
+				Server Team
+			</Title>
+			<List>
+				<List.Item>
+					<UnstyledButton
+						component="a"
+						href="https://github.com/Kariem816"
+						target="__blank"
+						rel="noopener noreferer"
+					>
+						Kareem Mostafa ElSawah
+					</UnstyledButton>
+				</List.Item>
+				<List.Item>
+					<UnstyledButton
+						component="a"
+						href="#"
+						target="__blank"
+						rel="noopener noreferer"
+					>
+						Mostafa Sabry Sayed
+					</UnstyledButton>
+				</List.Item>
+				<List.Item>
+					<UnstyledButton
+						component="a"
+						href="#"
+						target="__blank"
+						rel="noopener noreferer"
+					>
+						Youssef Mohammed Ibrahim
+					</UnstyledButton>
+				</List.Item>
+			</List>
+			<Title my="md" order={3}>
+				Embedded Team
+			</Title>
+			<List>
+				<List.Item>
+					<UnstyledButton
+						component="a"
+						href="#"
+						target="__blank"
+						rel="noopener noreferer"
+					>
+						Mohamed Sayed Farouk
+					</UnstyledButton>
+				</List.Item>
+				<List.Item>
+					<UnstyledButton
+						component="a"
+						href="#"
+						target="__blank"
+						rel="noopener noreferer"
+					>
+						Raghda Agmed Ahmed
+					</UnstyledButton>
+				</List.Item>
+				<List.Item>
+					<UnstyledButton
+						component="a"
+						href="#"
+						target="__blank"
+						rel="noopener noreferer"
+					>
+						Salah Adel Mohamed
+					</UnstyledButton>
+				</List.Item>
+			</List>
+			<Title my="md" order={3}>
+				Image Processing Team
+			</Title>
+			<List>
+				<List.Item>
+					<UnstyledButton
+						component="a"
+						href="#"
+						target="__blank"
+						rel="noopener noreferer"
+					>
+						Eslam Mohamed Hekal
+					</UnstyledButton>
+				</List.Item>
+				<List.Item>
+					<UnstyledButton
+						component="a"
+						href="#"
+						target="__blank"
+						rel="noopener noreferer"
+					>
+						Kareem Youssry Abdelaziz
+					</UnstyledButton>
+				</List.Item>
+				<List.Item>
+					<UnstyledButton
+						component="a"
+						href="#"
+						target="__blank"
+						rel="noopener noreferer"
+					>
+						Omar Gamal Abdelghaffar
+					</UnstyledButton>
+				</List.Item>
+			</List>
+			<Title my="md" order={2}>
+				Supervisor
+			</Title>
+			<UnstyledButton
+				component="a"
+				href="https://eng.asu.edu.eg/staff/helsayed"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				Prof. Dr. Hussein A. Elsayed
+			</UnstyledButton>
+			<Space h="xl" />
+		</>
 	);
 }

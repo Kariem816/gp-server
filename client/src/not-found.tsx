ed5a5d644c48613c052eaf56545d3efa19aba729
Kariem816 <kariem816@gmail.com>
2024-01-16 04:08:18 +0200

    C: update not found page

--- client/src/not-found.tsx
+++ client/src/not-found.tsx
@@ -1,16 +1,21 @@
-import React from "react";
-import { NotFoundRoute } from "@tanstack/react-router";
+import { Link, NotFoundRoute } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
+import { Title, Text, Stack, Button } from "@mantine/core";
 
 export const Route = new NotFoundRoute({
 	component: NotFound,
 	getParentRoute: () => routeTree,
 });
 
-function NotFound() {
+export function NotFound() {
 	return (
-		<div className="section">
-			<h1>404 | Not Found</h1>
-		</div>
+		<Stack align="center" py={150}>
+			<Title order={1}>404</Title>
+			<Title order={2}>Page not found</Title>
+			<Text>The page you are looking for does not exist.</Text>
+			<Button component={Link} to="/">
+				Go home
+			</Button>
+		</Stack>
 	);
 }

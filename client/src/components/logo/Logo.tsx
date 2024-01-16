405dfdf496b64398e96dc2e5f280a314b9e46dcf
Kariem816 <kariem816@gmail.com>
2024-01-16 23:40:54 +0200

    C: view text in logo in large screens only

--- client/src/components/logo/Logo.tsx
+++ client/src/components/logo/Logo.tsx
@@ -1,6 +1,7 @@
 import LogoImg from "~/assets/logo.svg";
 import styles from "./style.module.css";
 import { classnames } from "~/utils";
+import { Text } from "@mantine/core";
 
 type LogoProps = {
 	className?: string;
@@ -11,7 +12,7 @@ function Logo({ className, size }: LogoProps) {
 	return (
 		<div className={classnames(styles.logo, className)}>
 			<img src={LogoImg} alt="logo" height={size} width={size} />
-			<p>Smart Campus</p>
+			<Text visibleFrom="sm">Smart Campus</Text>
 		</div>
 	);
 }

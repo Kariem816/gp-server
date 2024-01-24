e2775478785a77b2d8a1a0c25c171c406000b6a3
Kariem816 <kariem816@gmail.com>
2024-01-24 20:58:41 +0200

    S: small change to env loading

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import { env } from "@/config/env.js";
 
 const globalForPrisma = globalThis as unknown as {
 	prisma: PrismaClient | undefined;
@@ -8,12 +9,12 @@ export const prisma =
 	globalForPrisma.prisma ??
 	new PrismaClient({
 		log:
-			process.env.NODE_ENV === "development"
+			env.NODE_ENV === "development"
 				? ["query", "error", "warn"]
 				: ["error"],
 	});
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export type PrismaClientError = {
 	code?: string;

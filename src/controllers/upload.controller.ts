4a0ca82d71ee1763b8e802a24935e2b8426e6825
Kariem816 <kariem816@gmail.com>
2024-01-24 21:26:13 +0200

    S: Clean upload controller

---
+++ src/controllers/upload.controller.ts
@@ -0,0 +1,66 @@
+import { ut, utapi } from "@/config/uploads.js";
+import userStore from "@/models/users.model.js";
+import uploadStore from "@/models/uploads.model.js";
+
+import {
+	createUploadthingExpressHandler,
+	type FileRouter,
+} from "uploadthing/express";
+
+// Define endpoints for UploadThing
+// "endpoint": ut({ options }).middleware(middleware).onUploadComplete(onUploadComplete)
+const uploadRouter = {
+	profilePics: ut({
+		image: {
+			maxFileSize: "4MB",
+			maxFileCount: 1,
+		},
+	})
+		.middleware(({ res }) => {
+			if (!res.locals.user) {
+				throw {
+					error: "UNAUTHORIZED",
+					message:
+						"You must be logged in to upload a profile picture",
+				};
+			}
+			return { userId: res.locals.user.id };
+		})
+		.onUploadComplete(async ({ file, metadata }) => {
+			// save upload data to database
+			// update user profile pic
+			// delete old profile pic
+			await uploadStore.create({
+				...file,
+				metadata,
+			});
+			const oldImg = await userStore.updateProfilePic(
+				metadata.userId,
+				file.url
+			);
+
+			try {
+				let oldImgKey: string | undefined;
+
+				if (oldImg) {
+					oldImgKey =
+						(await uploadStore.deleteByURL(oldImg))?.key ??
+						undefined;
+				}
+
+				if (oldImgKey) {
+					await utapi.deleteFiles(oldImgKey);
+				}
+			} catch (err) {
+				console.error(err);
+			}
+
+			return { img: file.url };
+		}),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof uploadRouter;
+
+export const uploadController = createUploadthingExpressHandler({
+	router: uploadRouter,
+});
--- src/routers/api/uploads.router.ts
+++ src/routers/api/uploads.router.ts
@@ -1,21 +1,14 @@
 import { Router, json } from "express";
-
-import { createUploadthingExpressHandler } from "uploadthing/express";
-
-import { uploadRouter } from "@/config/uploads.js";
+import { uploadController } from "@/controllers/upload.controller.js";
 
 const router = Router();
 
 // Why can't UploadThing send correct content-type header?
-router.use((req, res, next) => {
+router.use((req, _res, next) => {
 	req["headers"]["content-type"] = "application/json";
 	next();
 }, json());
 
-router.use(
-	createUploadthingExpressHandler({
-		router: uploadRouter,
-	})
-);
+router.use(uploadController);
 
 export default router;

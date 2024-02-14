c552e248fa09800b0de94ec9d41145400d948d8e
Kariem816 <kariem816@gmail.com>
2024-02-14 19:44:26 +0200

    S: leave client assets serving for express

--- src/routers/web/index.router.ts
+++ src/routers/web/index.router.ts
@@ -1,19 +1,11 @@
-import fs from "fs";
-import fsp from "fs/promises";
 import path from "path";
-import { Response, Router } from "express";
+import { Router, static as static_ } from "express";
 import { env } from "@/config/env.js";
 
 // Constants
 const isProduction = env.NODE_ENV === "production";
-const base = process.env.BASE || "/";
 
 const router = Router();
-
-// Cached production assets
-const templateHtml = isProduction
-	? await fsp.readFile("./dist/client/index.html", "utf-8")
-	: "";
 
 let vite: any;
 // Add Vite or respective production middlewares
@@ -23,40 +15,11 @@ if (!isProduction) {
 		root: path.resolve(process.cwd(), "./client/"),
 		server: { middlewareMode: true },
 		appType: "spa",
-		base,
+		base: "/",
 	});
 	router.use(vite.middlewares);
+} else {
+	router.use(static_(path.join(process.cwd(), "dist", "client")));
 }
-
-function renderIndex(res: Response, status = 200) {
-	res.status(status).set({ "Content-Type": "text/html" }).end(templateHtml);
-}
-
-router.get("*", async (req, res) => {
-	try {
-		if (!isProduction) {
-			throw new Error("Unreachable");
-		}
-
-		const url = req.originalUrl;
-		const segments = url.split("/").filter(Boolean);
-		if (segments.length === 0) {
-			return renderIndex(res);
-		}
-
-		const filePath = path.join("./dist/client", ...segments);
-		if (fs.existsSync(filePath) && fs.lstatSync(filePath).isFile()) {
-			res.sendFile(path.resolve(process.cwd(), filePath));
-		} else if (filePath.includes(".")) {
-			renderIndex(res, 404);
-		} else {
-			renderIndex(res);
-		}
-	} catch (err: any) {
-		vite?.ssrFixStacktrace(err);
-		console.log(err.stack);
-		renderIndex(res, 500);
-	}
-});
 
 export default router;

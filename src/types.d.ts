e2426a52560d5672ebb8aecc306f6121d225023f
Kariem816 <kariem816@gmail.com>
2024-02-24 05:13:25 +0200

    S: new utility type

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,5 +1,7 @@
 type QueryFilters = Record<string, any>;
 
+declare type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
+
 declare type PaginatedResponse<T = any> = {
 	data: T[];
 	total: number;

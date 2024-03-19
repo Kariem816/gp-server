2ed9439ce11e94046c70e0677e59c12aed8a3d51
Kariem816 <kariem816@gmail.com>
2024-03-19 15:12:18 +0200

    C: better syntax auth components

--- client/src/components/auth/index.tsx
+++ client/src/components/auth/index.tsx
@@ -1,1 +1,35 @@
-export { SignedIn, SignedOut, SignedInAs } from "./components";
+import { useAuth } from "~/contexts/auth";
+
+export function SignedIn({ children }: { children: React.ReactNode }) {
+	const { isLoggedIn } = useAuth();
+
+	if (!isLoggedIn) return null;
+
+	return children;
+}
+
+export function SignedOut({ children }: { children: React.ReactNode }) {
+	const { isLoggedIn } = useAuth();
+
+	if (isLoggedIn) return null;
+
+	return children;
+}
+
+export function SignedInAs({
+	children,
+	role,
+}: {
+	children: React.ReactNode;
+	role: string | string[];
+}) {
+	const { isLoggedIn, user } = useAuth();
+
+	if (!isLoggedIn) return null;
+
+	if (Array.isArray(role) && role.includes(user.role)) {
+		return children;
+	} else if (user.role === role) {
+		return children;
+	}
+}

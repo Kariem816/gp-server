0e698459c636a9740da4cf04953c5a84f7c3536d
Kariem816 <kariem816@gmail.com>
2024-03-19 14:53:31 +0200

    C: Restore 404 page

--- client/src/not-found.tsx
+++ client/src/not-found.tsx
@@ -1,6 +1,7 @@
 import { Link, NotFoundRoute } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 import { Button } from "./components/ui/button";
+import { HomeIcon } from "@radix-ui/react-icons";
 
 export const Route = new NotFoundRoute({
 	component: NotFound,
@@ -9,14 +10,15 @@ export const Route = new NotFoundRoute({
 
 export function NotFound() {
 	return (
-		<div className="flex flex-col items-center">
-			<h1 className="text-center">404</h1>
-			<h2 className="text-center">Page not found</h2>
-			<p className="text-center">
-				The page you are looking for does not exist.
-			</p>
-			<Button size="lg">
-				<Link to="/">Go home</Link>
+		<div className="h-full flex flex-col items-center justify-center text-center">
+			<h1>404</h1>
+			<h2>Page not found</h2>
+			<p>The page you are looking for does not exist.</p>
+			<Button size="lg" asChild>
+				<Link to="/" className="mt-4">
+					<HomeIcon className="w-6 h-6 mr-2" />
+					Go home
+				</Link>
 			</Button>
 		</div>
 	);

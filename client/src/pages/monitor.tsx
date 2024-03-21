6735a312eee8c362939f68a757a90bc3f4012cdb
Kariem816 <kariem816@gmail.com>
2024-03-21 14:00:37 +0200

    C: who seriously makes resources in a loop

--- client/src/pages/monitor.tsx
+++ client/src/pages/monitor.tsx
@@ -1,5 +1,18 @@
-import { createFileRoute, Link, Outlet } from "@tanstack/react-router";
+import {
+	createFileRoute,
+	Link,
+	Outlet,
+	useNavigate,
+} from "@tanstack/react-router";
 import { get } from "~/services/api";
+import {
+	Collapsible,
+	CollapsibleContent,
+	CollapsibleTrigger,
+} from "~/components/ui/collapsible";
+import { Button } from "~/components/ui/button";
+import { useState } from "react";
+import { CaretSortIcon } from "@radix-ui/react-icons";
 
 export const Route = createFileRoute("/monitor")({
 	component: MonitorLayout,
@@ -11,6 +24,17 @@ export const Route = createFileRoute("/monitor")({
 
 function MonitorLayout() {
 	const controllers = Route.useLoaderData();
+	const [isOpen, setIsOpen] = useState(false);
+	const navigate = useNavigate();
+
+	function onControllerClick(id: number) {
+		setIsOpen(false);
+		navigate({
+			to: "/monitor/$id",
+			params: { id: id + "" },
+		});
+	}
+
 	return (
 		<div className="h-full flex flex-col divide-x-2 md:flex-row">
 			<div className="p-4 md:w-[25%] md:h-full md:overflow-auto">
@@ -20,18 +44,51 @@ function MonitorLayout() {
 						No controllers available
 					</p>
 				) : (
-					<ul>
-						{controllers.map((controller) => (
-							<li key={controller.id}>
-								<Link
-									to={"/monitor/$id"}
-									params={{ id: controller.id + "" }}
-								>
-									{controller.label}
-								</Link>
-							</li>
-						))}
-					</ul>
+					<>
+						<ul className="hidden md:block">
+							{controllers.map((controller) => (
+								<li key={controller.id}>
+									<Link
+										to={"/monitor/$id"}
+										params={{ id: controller.id + "" }}
+									>
+										{controller.label}
+									</Link>
+								</li>
+							))}
+						</ul>
+						<Collapsible
+							open={isOpen}
+							onOpenChange={setIsOpen}
+							className="md:hidden space-y-2"
+						>
+							<div className="flex items-center justify-between space-x-4 px-4">
+								<h4 className="text-sm font-semibold">
+									Available Controllers
+								</h4>
+								<CollapsibleTrigger asChild>
+									<Button variant="ghost" size="sm">
+										<CaretSortIcon className="h-4 w-4" />
+										<span className="sr-only">Toggle</span>
+									</Button>
+								</CollapsibleTrigger>
+							</div>
+							<CollapsibleContent className="space-x-2 space-y-2 bg-muted rounded-md p-4">
+								{controllers.map((controller) => (
+									<Button
+										key={controller.id}
+										variant="outline"
+										size="sm"
+										onClick={() =>
+											onControllerClick(controller.id)
+										}
+									>
+										{controller.label}
+									</Button>
+								))}
+							</CollapsibleContent>
+						</Collapsible>
+					</>
 				)}
 			</div>
 			<div className="container py-8 md:h-full md:overflow-auto">

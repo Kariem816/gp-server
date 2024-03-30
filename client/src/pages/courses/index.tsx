58c783f05a74c2875773dd5e9652959ab018dc32
Kariem816 <kariem816@gmail.com>
2024-03-30 23:53:54 +0200

    C: finalize courses page

--- client/src/pages/courses/index.tsx
+++ client/src/pages/courses/index.tsx
@@ -1,11 +1,15 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { useState } from "react";
 import { CourseListing } from "~/components/courses/course-listing";
+import { Spinner } from "~/components/loaders";
+import { Pagination } from "~/components/pagination";
 import { Input } from "~/components/ui/input";
 import useDebounce from "~/hooks/use-debounce";
 import { usePaginatedQuery } from "~/hooks/use-paginated-query";
 import { getCourses } from "~/services/courses";
-import { filterize } from "~/utils";
+import { cn, filterize } from "~/utils";
+import { UpdateIcon } from "@radix-ui/react-icons";
+import { Button } from "~/components/ui/button";
 
 export const Route = createFileRoute("/courses/")({
 	component: CoursesList,
@@ -18,12 +22,14 @@ function CoursesList() {
 	const debouncedFilters = useDebounce(filters, 1000);
 	const {
 		data: courses,
+		refetch,
+		isRefetching,
 		isPending,
 		isError,
 		error,
 		page,
 		pages,
-		// setPage,
+		setPage,
 	} = usePaginatedQuery({
 		queryKey: ["courses", debouncedFilters.name],
 		queryFn: (pagination) =>
@@ -39,32 +45,48 @@ function CoursesList() {
 
 	return (
 		<div className="container flex flex-col h-full">
-			<div className="flex items-center justify-between flex-wrap">
-				<h1>Courses</h1>
-				<div className="flex items-center gap-2">
-					<span className="text-nowrap">
-						Page {page} of {pages}
-					</span>
-					<Input
-						placeholder="Search by name"
-						value={filters.name}
-						onChange={(e) =>
-							setFilters((prev) => ({
-								...prev,
-								name: e.target.value,
-							}))
-						}
-					/>
+			<div className="flex flex-col justify-center border-b-2">
+				<div className="flex items-center justify-between flex-wrap gap-2">
+					<div className="flex gap-2 items-center">
+						<h1 className="my-1">Courses</h1>
+						<Button
+							variant="ghost"
+							size="icon"
+							onClick={() => refetch()}
+						>
+							<UpdateIcon
+								className={cn(
+									"text-primary",
+									isRefetching && "animate-spin"
+								)}
+							/>
+						</Button>
+					</div>
+					<div className="flex flex-grow xs:flex-grow-0 mb-2 xs:mb-0">
+						<Input
+							placeholder="Search by name"
+							value={filters.name}
+							onChange={(e) =>
+								setFilters((prev) => ({
+									...prev,
+									name: e.target.value,
+								}))
+							}
+						/>
+					</div>
+				</div>
+				<div className="mb-2">
+					<Pagination page={page} pages={pages} onChange={setPage} />
 				</div>
 			</div>
-			<div className="flex-grow overflow-auto pe-1 md:pe-2 mb-2">
+			<div className="flex-grow overflow-auto pe-1 md:pe-2 my-2">
 				{isError ? (
-					<div className="text-destructive italic font-semibold">
+					<div className="text-destructive italic font-semibold h-full flex items-center justify-center">
 						{error.message}
 					</div>
 				) : isPending ? (
 					<div className="h-full flex justify-center items-center">
-						<div className="loader" />
+						<Spinner />
 					</div>
 				) : (
 					<ul>

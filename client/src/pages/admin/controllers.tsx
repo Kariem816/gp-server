17a4e68bd58616952651204cce33f968ff4eff82
Kariem816 <kariem816@gmail.com>
2024-03-30 21:40:16 +0200

    C: one more placeholder

--- client/src/pages/admin/controllers.tsx
+++ client/src/pages/admin/controllers.tsx
@@ -5,5 +5,9 @@ export const Route = createFileRoute("/admin/controllers")({
 });
 
 function AdminControllers() {
-	return <div>placeholder</div>;
+	return (
+		<div className="text-center h-full flex justify-center items-center font-bold text-2xl">
+			Under Construction
+		</div>
+	);
 }

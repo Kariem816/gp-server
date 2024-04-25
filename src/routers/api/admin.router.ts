3ac2ba121436a8ab1ab7a92bd082398569df5441
Kariem816 <kariem816@gmail.com>
2024-04-25 22:50:29 +0200

    S: bye bye kitty

--- src/routers/api/admin.router.ts
+++ src/routers/api/admin.router.ts
@@ -6,7 +6,7 @@ const router = Router();
 router.get("/parking-camera", mustBe("admin"), (_req, res) => {
 	res.json({
 		data: {
-			img: "https://t4.ftcdn.net/jpg/00/97/58/97/240_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg",
+			img: "https://utfs.io/f/436eb844-bc86-4a0c-b62e-cb0f7d0524f3-sh5vlw.jpg",
 		},
 	});
 });

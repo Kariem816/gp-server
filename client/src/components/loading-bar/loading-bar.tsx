413ff903a4df984baf9c16f853878651290acc42
hekal74 <eslamhekal74@gmail.com>
2024-05-01 23:15:12 +0300

    edit app pages

--- client/src/components/loading-bar/loading-bar.tsx
+++ client/src/components/loading-bar/loading-bar.tsx
@@ -6,11 +6,13 @@ function LoadingBar({ value }: { value: number }) {
 			<div
 				className={cn(
 					"h-full transition-all duration-500 ease-in-out",
-					value > 33
-						? value > 66
-							? "bg-destructive"
-							: "bg-orange-600"
-						: "bg-primary"
+					value > 33?
+						 value > 66?
+						 value > 80?
+							 "bg-red-500"
+							 :"bg-orange-500"
+						: "bg-yellow-500"
+					: "bg-green-500	"
 				)}
 				style={{
 					width: `${value}%`,
@@ -19,5 +21,6 @@ function LoadingBar({ value }: { value: number }) {
 		</div>
 	);
 }
+
 
 export default LoadingBar;

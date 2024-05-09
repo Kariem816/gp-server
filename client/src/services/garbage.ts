2136d443d8709223e870265b5ac5a66c004b343f
Kariem816 <kariem816@gmail.com>
2024-05-09 15:10:12 +0300

    C: update garbage schema

--- client/src/services/garbage.ts
+++ client/src/services/garbage.ts
@@ -3,11 +3,14 @@ import { get } from "./api";
 import type { APIQuery } from "~/types/query";
 
 export type TrashCan = {
-    id: string;
-    level: number;
-    location: string;
-}
+	id: string;
+	level: number;
+	location: string;
+	lastEmptied: string; // ISO Date
+};
 
-export async function getAllCans(query: APIQuery = {}): Promise<PaginatedResponse<TrashCan>> {
-    return get("/trash", query);
+export async function getAllCans(
+	query: APIQuery = {}
+): Promise<PaginatedResponse<TrashCan>> {
+	return get("/trash", query);
 }

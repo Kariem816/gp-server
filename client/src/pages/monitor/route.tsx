43c34c2f87826dfeb4bdaa62c03b9957cb103989
Kariem816 <kariem816@gmail.com>
2024-05-11 03:30:11 +0300

    C: translate monitor page

--- client/src/pages/monitor/route.tsx
+++ client/src/pages/monitor/route.tsx
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { createFileRoute, Link, Outlet } from "@tanstack/react-router";
 import { Spinner } from "~/components/loaders";
 import { useAuth } from "~/contexts/auth";
+import { useTranslation } from "~/contexts/translation";
 import { useSecurePage } from "~/hooks/use-secure-page";
 import { getMyPermissions } from "~/services/controllers";
 
@@ -11,6 +12,7 @@ export const Route = createFileRoute("/monitor")({
 
 function MonitorLayout() {
 	const { user } = useAuth();
+	const { t } = useTranslation();
 
 	const {
 		data: permissions,
@@ -28,7 +30,7 @@ function MonitorLayout() {
 	return (
 		<div className="h-full flex flex-col divide-x-2 md:flex-row">
 			<div className="p-4 md:w-[25%] md:h-full md:overflow-auto">
-				<h2>Monitor</h2>
+				<h2>{t("monitor")}</h2>
 				{isError ? (
 					<div className="text-destructive italic font-semibold h-full flex items-center justify-center">
 						{error.message}
@@ -45,7 +47,7 @@ function MonitorLayout() {
 								className="hover:underline"
 								to="/monitor/garbage"
 							>
-								Garbage
+								{t("garbage")}
 							</Link>
 						) : null}
 						{user.role === "admin" ||
@@ -54,7 +56,7 @@ function MonitorLayout() {
 								className="hover:underline"
 								to="/monitor/parking"
 							>
-								Parking
+								{t("parking")}
 							</Link>
 						) : null}
 						{user.role === "admin" ||
@@ -63,7 +65,7 @@ function MonitorLayout() {
 								className="hover:underline"
 								to="/monitor/smart-parking"
 							>
-								Smart Parking
+								{t("smart_parking")}
 							</Link>
 						) : null}
 						{user.role === "admin" ||
@@ -72,7 +74,7 @@ function MonitorLayout() {
 								className="hover:underline"
 								to="/monitor/irrigation"
 							>
-								Irrigation
+								{t("irrigation")}
 							</Link>
 						) : null}
 						{user.role === "admin" ||
@@ -81,7 +83,7 @@ function MonitorLayout() {
 								className="hover:underline"
 								to="/monitor/lighting"
 							>
-								Lighting
+								{t("lighting")}
 							</Link>
 						) : null}
 					</div>

e0ea657c606ad38de2eb2a8d0a6a874717c81914
Kariem816 <kariem816@gmail.com>
2024-06-20 17:06:34 +0300

    C: remove redundant variable

--- client/src/pages/monitor/irrigation/index.tsx
+++ client/src/pages/monitor/irrigation/index.tsx
@@ -1,20 +1,18 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { useQuery } from "@tanstack/react-query";
-import { getAllPlants } from "~/services/irrigation.ts";
-import PlantCard from "../../../components/irrigation/index.tsx";
+import { getAllPlants } from "~/services/irrigation";
+import PlantCard from "~/components/irrigation";
 import { Spinner } from "~/components/loaders";
 import { time } from "~/utils/formatters/time";
-import { useTranslation } from "~/contexts/translation";
-import { useControllerPermission } from "~/hooks/controllers/use-controller-permission.ts";
-import { NewPlanetSpot } from "~/components/irrigation/new.tsx";
-import { PlanetSpot } from "~/components/irrigation/delete.tsx";
+import { useControllerPermission } from "~/hooks/controllers/use-controller-permission";
+import { NewPlanetSpot } from "~/components/irrigation/new";
+import { PlanetSpot } from "~/components/irrigation/delete";
 
 export const Route = createFileRoute("/monitor/irrigation/")({
 	component: IrrigationList,
 });
 function IrrigationList() {
 	useControllerPermission("/", "irrigation");
-	const { t } = useTranslation();
 	const {
 		data: irrigation,
 		isLoading,
@@ -26,7 +24,6 @@ function IrrigationList() {
 		select: (data) => data.data,
 		refetchInterval: 3 * 1000, // 3 seconds
 	});
-
 
 	if (isError) {
 		return (
@@ -45,35 +42,31 @@ function IrrigationList() {
 
 	return (
 		<>
-		<div>
-			<div className="flex justify-end mb-2">
-				<NewPlanetSpot />
-			</div>
-			{irrigation.length === 0 ? (
-				<p className="italic text-center">No data available</p>
-			) : (
-				irrigation.map((plant) => (
-					
+			<div>
+				<div className="flex justify-end mb-2">
+					<NewPlanetSpot />
+				</div>
+				{irrigation.length === 0 ? (
+					<p className="italic text-center">No data available</p>
+				) : (
+					irrigation.map((plant) => (
 						<div
-						
-						key={plant.id}
-						className="rounded-lg bg-accent p-4 space-y-4 mb-8"
-					>
-						<div className="flex justify-end">
-							<PlanetSpot key={plant.id} plant={plant}/>
-							
+							key={plant.id}
+							className="rounded-lg bg-accent p-4 space-y-4 mb-8"
+						>
+							<div className="flex justify-end">
+								<PlanetSpot key={plant.id} plant={plant} />
+							</div>
+
+							<PlantCard
+								id={plant.id}
+								lastUpdate={time(plant.lastUpdated)}
+								isWatering={plant.isWatering}
+								plantType={plant.type}
+							/>
 						</div>
-	
-						<PlantCard
-							id={plant.id}
-							lastUpdate={time(plant.lastUpdated)}
-							isWatering={plant.isWatering}
-							plantType={plant.type}
-						/>
-					</div>
-					
-				))
-			)}
+					))
+				)}
 			</div>
 		</>
 	);

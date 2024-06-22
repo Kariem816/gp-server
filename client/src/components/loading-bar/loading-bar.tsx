d19680682add6268f3b5a2fd3bb37fac9580259b
Kariem816 <kariem816@gmail.com>
2024-06-23 01:12:26 +0300

    C: loading bar with words

--- client/src/components/loading-bar/loading-bar.tsx
+++ client/src/components/loading-bar/loading-bar.tsx
@@ -2,25 +2,28 @@ import { cn } from "~/utils";
 
 function LoadingBar({ value }: { value: number }) {
 	return (
-		<div className="h-5 bg-accent rounded-full border border-accent-foreground overflow-hidden">
+		<div className="h-5 bg-accent rounded-full border border-accent-foreground overflow-hidden relative">
 			<div
 				className={cn(
 					"h-full transition-all duration-500 ease-in-out",
-					value > 33?
-						 value > 66?
-						 value > 80?
-							 "bg-red-500"
-							 :"bg-orange-500"
-						: "bg-yellow-500"
-					: "bg-green-500	"
+					value > 33
+						? value > 66
+							? value > 80
+								? "bg-red-500 text-white"
+								: "bg-orange-500 text-white"
+							: "bg-yellow-500 text-red-800"
+						: "bg-green-500"
 				)}
 				style={{
 					width: `${value}%`,
 				}}
-			/>
+			>
+				<span className="absolute inset-0 grid place-items-center text-sm">
+					{value} %
+				</span>
+			</div>
 		</div>
 	);
 }
-
 
 export default LoadingBar;

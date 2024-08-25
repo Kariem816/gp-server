3a448539c53531067342e8356eac46c62ca5902f
Kariem816 <kariem816@gmail.com>
2024-08-25 19:20:52 +0300

    Shutdown the app 👋😭

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,44 @@
 import express from "express";
-import cookieParser from "cookie-parser";
-import cors from "cors";
+// import cookieParser from "cookie-parser";
+// import cors from "cors";
 
 import { env } from "@/config/env";
 
 import appRouter from "@/routers/index.router";
-import { getUser, logger } from "@/middlewares";
-import { rateLimiter } from "./middlewares/rate";
+// import { getUser, logger } from "@/middlewares";
+// import { rateLimiter } from "./middlewares/rate";
 
 const app = express();
 const port = env.PORT || 3000;
 
-app.use(express.json());
-//@ts-ignore
-app.use((error, _req, res, next) => {
-	if (error instanceof SyntaxError) {
-		res.status(400).json({
-			error: "BAD_REQUEST",
-			message: "Invalid payload",
-		});
-	} else {
-		next();
-	}
-});
-app.use(cookieParser());
-if (env.NODE_ENV === "development") {
-	app.use(
-		cors({
-			origin: env.CLIENT_DEV_SERVER,
-			credentials: true,
-		})
-	);
-}
+// app.use(express.json());
+// //@ts-ignore
+// app.use((error, _req, res, next) => {
+// 	if (error instanceof SyntaxError) {
+// 		res.status(400).json({
+// 			error: "BAD_REQUEST",
+// 			message: "Invalid payload",
+// 		});
+// 	} else {
+// 		next();
+// 	}
+// });
+// app.use(cookieParser());
+// if (env.NODE_ENV === "development") {
+// 	app.use(
+// 		cors({
+// 			origin: env.CLIENT_DEV_SERVER,
+// 			credentials: true,
+// 		})
+// 	);
+// }
 
-app.use(
-	logger({ debugOnly: true, fields: ["method", "path", "query", "body"] })
-);
+// app.use(
+// 	logger({ debugOnly: true, fields: ["method", "path", "query", "body"] })
+// );
 
-app.use(getUser);
-app.use(rateLimiter);
+// app.use(getUser);
+// app.use(rateLimiter);
 app.use(appRouter);
 
 app.listen(port, () => {
--- src/routers/index.router.ts
+++ src/routers/index.router.ts
@@ -8,7 +8,58 @@ router.get("/health", (_req, res) => {
 	res.sendStatus(200);
 });
 
-router.use("/api", apiRouter);
-router.use("/", webRouter);
+// router.use("/api", apiRouter);
+// router.use("/", webRouter);
+
+router.use("/api", (_req, res) => {
+	res.json({
+		message:
+			"The API has been shutdown due to lack of funds. We could not afford to keep it running. You can still access the code on GitHub (https://github.com/Kariem816/gp-server).",
+	});
+});
+router.use("/", (req, res) => {
+	res.send(`
+		<!DOCTYPE html>
+		<html lang="en">
+			<head>
+				<meta charset="UTF-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<title>Smart Campus (shutdown)</title>
+				<style>
+					*, *::before, *::after {
+						box-sizing: border-box;
+					}
+
+					:root {
+						--primary: hsl(221.2 83.2% 53.3%);
+						--muted: hsl(210 40% 96.1%);
+					}
+
+					body {
+						margin: 0;
+						padding: 0;
+						font-family: Arial, sans-serif;
+						display: flex;
+						flex-direction: column;
+						justify-content: center;
+						align-items: center;
+						gap: 3rem;
+						height: 100vh;
+						background-color: var(--muted);
+					}
+
+					h1 {
+						color: var(--primary);
+						text-align: center;
+					}
+				</style>
+			</head>
+			<body>
+				<h1>Smart Campus</h1>
+				<p>The website has been shutdown due to lack of funds. We could not afford to keep it running. You can still access the code on <a href="https://github.com/Kariem816/gp-server">GitHub</a>.</p>
+			</body>
+		</html>
+	`);
+});
 
 export default router;
